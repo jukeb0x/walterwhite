b60a60dea3eba36863838dfc3d513860830fa90a
mathilde1011@hotmail.fr <yannsamuell0>
2020-08-01 12:07:24 +0200

    Gestion du tab + navigation

--- app/src/main/java/fr/mm/walterwhite/views/MealsActivity.java
+++ app/src/main/java/fr/mm/walterwhite/views/MealsActivity.java
@@ -1,14 +1,23 @@
 package fr.mm.walterwhite.views;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -26,15 +35,18 @@ import fr.mm.walterwhite.models.Consommation;
 import fr.mm.walterwhite.views.models.ConsommationViewModel;
 import fr.mm.walterwhite.views.models.MealViewModel;
 
-public class MealsActivity extends AppCompatActivity {
+public class MealsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
 
     private ListView listConsos;
     private RecyclerView listMeals;
     private RecyclerView listDays;
-    private final List<Consommation> consoList = new ArrayList<Consommation>();
+    private final List<Consommation> consoList = new ArrayList<>();
     private ArrayAdapter<Consommation> listViewAdapter;
     private MealRecyclerViewAdapter listViewAdapterMeals;
     private DayRecyclerViewAdapter listViewAdapterDays;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
 
 
@@ -42,14 +54,34 @@ public class MealsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meals);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        handleDays();
+    /*    Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+       configureDrawerLayout();
+        configureNavigationView();
         handleMeals();
         handleConsos();
+        handleButton();
 
 
     }
+
+    // 2 - Configure Drawer Layout
+    private void configureDrawerLayout(){
+        this.drawerLayout = findViewById(R.id.activity_main_drawer_layout);
+       Toolbar toolbar = findViewById(R.id.toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    // 3 - Configure NavigationView
+    private void configureNavigationView(){
+        this.navigationView = findViewById(R.id.activity_main_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+
 
     protected void handleConsos() {
         // Get ListView object from xml
@@ -82,11 +114,24 @@ public class MealsActivity extends AppCompatActivity {
         registerForContextMenu(this.listConsos);*/
     }
 
+    protected void handleButton() {
+        Button myButton =  findViewById(R.id.button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MealsActivity.this, NewConsoActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
 
 
     protected void handleMeals() {
         // Get ListView object from xml
-        this.listMeals= (RecyclerView) findViewById(R.id.listMeals);
+        this.listMeals= findViewById(R.id.listMeals);
 
         LinearLayoutManager layoutManager
                 = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
@@ -105,7 +150,7 @@ public class MealsActivity extends AppCompatActivity {
         // Register the ListView for Context menu
         //registerForContextMenu(this.listMeals);
 
-        List<MealViewModel> listMModels =new  ArrayList<MealViewModel>();
+        List<MealViewModel> listMModels =new  ArrayList<>();
         for(String mealSel:Constants.MEALS) {
             double points=0;
             ConsommationDao db = new ConsommationDao(this);
@@ -116,7 +161,7 @@ public class MealsActivity extends AppCompatActivity {
             SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
             String formattedDate = df.format(c);
             List<Consommation> list = db.getConsommations(formattedDate, mealSel);
-            List<ConsommationViewModel> listCModels =new  ArrayList<ConsommationViewModel>();
+            List<ConsommationViewModel> listCModels =new  ArrayList<>();
             for(Consommation conso:list){
                 points+= conso.getEatenPoints();
                 ConsommationViewModel cm=new ConsommationViewModel(conso.getEatenName(),conso.getEatenPoints()+"",conso.getEatenPortion()+"gr");
@@ -142,7 +187,7 @@ public class MealsActivity extends AppCompatActivity {
 
     protected void handleDays() {
         // Get ListView object from xml
-        this.listDays = (RecyclerView) findViewById(R.id.listDays);
+        this.listDays =  findViewById(R.id.listDays);
 
         LinearLayoutManager layoutManager
                 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
@@ -168,6 +213,39 @@ public class MealsActivity extends AppCompatActivity {
         registerForContextMenu(this.listDays);
     }
 
+
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        // 5 - Handle back click to close menu
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        // 4 - Handle Navigation Item Click
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_settings:
+                break;
+            default:
+                break;
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
 
 
 }
